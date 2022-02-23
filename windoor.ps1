deeb1e532b51f7efb1b21bf11a97ee2d060a38c5
Talha Mehar <57623612+Talhamehar007@users.noreply.github.com>
2022-02-23 21:17:07 +0500

    Update windoor.ps1

--- windoor.ps1
+++ windoor.ps1
@@ -23,3 +23,11 @@ Get-NetFirewallRule -Name *ssh*
 # There should be a firewall rule named "OpenSSH-Server-In-TCP", which should be enabled. If the firewall does not exist, create one
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 
+# By default the ssh-agent service is disabled. Allow it to be Automatically started for the next step to work.
+Get-Service ssh-agent | Set-Service -StartupType Automatic
+
+# Start the service
+Start-Service ssh-agent
+
+# Now load your key files into ssh-agent
+ssh-add $HOME\.ssh\id_idrsa
